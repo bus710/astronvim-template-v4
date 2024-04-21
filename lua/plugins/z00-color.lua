5be9b7428639058c164a15f024d9f3a0c9db8b19
bus710 <bus710@gmail.com>
2024-04-21 14:55:02 -0700

    astronvim upgrade to v4

---
+++ lua/plugins/z00-color.lua
@@ -0,0 +1,23 @@
+-- https://www.reddit.com/r/AstroNvim/comments/1bu8dx6/astronvim_v4_single_file_conf/
+
+return { 
+  { 'Mofiqul/dracula.nvim' }, 
+  {
+    "AstroNvim/astroui",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      options = {
+        opt = {
+          tabstop = 4,
+          softtabstop = 4,
+          colorcolumn = '80,120'
+        },
+      },
+    },
+  },
+}
