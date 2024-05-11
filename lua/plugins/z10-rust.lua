f1157df87113e5383229be3835ca9d31e4558a44
bus710 <bus710@gmail.com>
2024-05-11 10:27:06 -0700

    progress

--- lua/plugins/z10-rust.lua
+++ lua/plugins/z10-rust.lua
@@ -7,7 +7,12 @@ If the dap-ui doesn't start well, check the LSP log
 - :LspInfo
 - :LspLog
 - :lua vim.lsp.buf.definition() on a token
+
+If Mason keeps failing codelldb, delete the lock file with
+- rm -rf $HOME/.local/share/nvim/mason/staging/codelldb.lock
 ]]--
+
+
 
 return {
   -- To launch the debugger
