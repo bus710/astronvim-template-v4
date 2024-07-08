b2521befc4b50f98fc68f275fcf8f46f973b16bf
bus710 <bus710@gmail.com>
2024-07-08 12:11:33 -0700

    progress

---
+++ lua/plugins/z51-tailwind-tools.lua
@@ -0,0 +1,9 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- This function will run when open an elixir file.
+return {
+  "luckasRanarison/tailwind-tools.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  opts = {} -- your configuration
+}
+
