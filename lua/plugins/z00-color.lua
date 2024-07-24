5db228dff154d32d9afcd4aa483fe2f9bcd11560
bus710 <bus710@gmail.com>
2024-07-24 10:58:47 -0700

    textwidth

--- lua/plugins/z00-color.lua
+++ lua/plugins/z00-color.lua
@@ -1,7 +1,7 @@
 -- https://www.reddit.com/r/AstroNvim/comments/1bu8dx6/astronvim_v4_single_file_conf/
 
-return { 
-  { 'Mofiqul/dracula.nvim' }, 
+return {
+  { 'Mofiqul/dracula.nvim' },
   {
     "AstroNvim/astroui",
     opts = {
@@ -16,7 +16,9 @@ return {
           tabstop = 4,
           softtabstop = 4,
           -- colorcolumn = '80,120'
-          colorcolumn = '120'
+          colorcolumn = '120',
+          textwidth = 100,
+          wrap = true
         },
       },
     },
