9981d9de834235c63845767063c285eb33a63fbf
bus710 <bus710@gmail.com>
2024-08-16 14:13:18 -0700

    progress

--- lua/plugins/z20-zig.lua
+++ lua/plugins/z20-zig.lua
@@ -1,50 +1,89 @@
 -- https://terminalprogrammer.com/neovim-setup-for-zig
 -- :lua print(vim.fn.getcwd())
 
+-- return {
+--   {
+--     "mfussenegger/nvim-dap",
+--
+--     config = function()
+--       local dap, dapui = require "dap", require "dapui"
+--
+--       -- sudo apt install lldb (for the command)
+--       dap.adapters.lldb = {
+--         type = 'executable',
+--         command = '/usr/bin/lldb-vscode',
+--         name = 'lldb'
+--       }
+--       dap.configurations.zig = {
+--         {
+--           name = 'Launch',
+--           type = 'lldb',
+--           request = 'launch',
+--           program = '${workspaceFolder}/zig-out/bin/${workspaceFolderBasename}',
+--           -- program = "${file}";
+--           -- program = vim.fn.getcwd() .. '/zig-out/bin/${workspaceFolderBasename}',
+--           -- program = function()
+--           --   return vim.fn.input({
+--           --     prompt = 'Path to executable for zig: ',
+--           --     -- default = vim.fn.getcwd() .. '/',
+--           --     defailt = '${workspaceFolder}/zig-out/bin/${workspaceFolderBasename}',
+--           --     completion = 'file',
+--           --   })
+--           -- end,
+--           cwd = '${workspaceFolder}',
+--           stopOnEntry = false,
+--           args = {},
+--         }
+--       }
+--
+--       dap.listeners.after.event_initialized["dapui_config"] = function()
+--         dapui.open({})
+--       end
+--       dap.listeners.before.event_terminated["dapui_config"] = function()
+--         dapui.close({})
+--       end
+--       dap.listeners.before.event_exited["dapui_config"] = function()
+--         dapui.close({})
+--       end
+--     end,
+--   },
+-- }
+
 return {
-  {
-    "mfussenegger/nvim-dap",
+  "mfussenegger/nvim-dap",
 
-    config = function()
-      local dap, dapui = require "dap", require "dapui"
-
-      -- sudo apt install lldb (for the command)
-      dap.adapters.lldb = {
-        type = 'executable',
-        command = '/usr/bin/lldb-vscode',
-        name = 'lldb'
-      }
-      dap.configurations.zig = {
-        {
-          name = 'Launch',
-          type = 'lldb',
-          request = 'launch',
-          program = '${workspaceFolder}/zig-out/bin/${workspaceFolderBasename}',
-          -- program = "${file}";
-          -- program = vim.fn.getcwd() .. '/zig-out/bin/${workspaceFolderBasename}',
-          -- program = function()
-          --   return vim.fn.input({
-          --     prompt = 'Path to executable for zig: ',
-          --     -- default = vim.fn.getcwd() .. '/',
-          --     defailt = '${workspaceFolder}/zig-out/bin/${workspaceFolderBasename}',
-          --     completion = 'file',
-          --   })
-          -- end,
-          cwd = '${workspaceFolder}',
-          stopOnEntry = false,
-          args = {},
+  opts = {
+    handlers = {
+      zig = function()
+        local dap, dapui = require "dap", require "dapui"
+        -- sudo apt install lldb (for the command)
+        dap.adapters.lldb = {
+          type = 'executable',
+          command = '/usr/bin/lldb-vscode',
+          name = 'lldb'
         }
-      }
+        dap.configurations.zig = {
+          {
+            name = 'Launch',
+            type = 'lldb',
+            request = 'launch',
+            program = '${workspaceFolder}/zig-out/bin/${workspaceFolderBasename}',
+            cwd = '${workspaceFolder}',
+            stopOnEntry = false,
+            args = {},
+          }
+        }
 
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open({})
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close({})
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close({})
-      end
-    end,
+        dap.listeners.after.event_initialized["dapui_config"] = function()
+          dapui.open({})
+        end
+        dap.listeners.before.event_terminated["dapui_config"] = function()
+          dapui.close({})
+        end
+        dap.listeners.before.event_exited["dapui_config"] = function()
+          dapui.close({})
+        end
+      end,
+    },
   },
 }
