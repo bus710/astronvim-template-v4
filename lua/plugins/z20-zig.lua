bd53dc095d585a1401fd35603971a6b6d0365dea
bus710 <bus710@gmail.com>
2024-08-17 20:26:58 -0700

    progress

--- lua/plugins/z20-zig.lua
+++ lua/plugins/z20-zig.lua
@@ -4,7 +4,47 @@
 
 -- if true then return {} end
 
+
 return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = function(_, opts)
+      if opts.ensure_installed ~= "all" then
+        opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "zig" })
+      end
+    end,
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = { "lawrence-laz/neotest-zig", version = "^1" },
+    opts = function(_, opts)
+      if not opts.adapters then opts.adapters = {} end
+      table.insert(opts.adapters, require "neotest-zig")
+    end,
+  },
+  {
+    "https://codeberg.org/NTBBloodbath/zig-tools.nvim",
+    -- Load zig-tools.nvim only in Zig buffers
+    ft = { "zig" },
+    opts = {},
+    dependencies = {
+      "akinsho/toggleterm.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+  },
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    ---@type AstroLSPOpts
+    opts = {
+      -- servers = { "zls" },
+    },
+  },
+
+  --------------------------------------------------
+
   {
     "mfussenegger/nvim-dap",
     config = function(_, _)
@@ -21,10 +61,10 @@ return {
           name = "Launch",
           type = "lldb",
           request = "launch",
-          program = "${workspaceFolder}/zig-out/bin/${workspaceFolderBasename}",
-          -- program = function()
-          --   return vim.fn.input('Path to executable : ', vim.fn.getcwd() .. '/zig-out/bin/', 'file')
-          -- end,
+          -- program = "${workspaceFolder}/zig-out/bin/${workspaceFolderBasename}",
+          program = function()
+            return vim.fn.input('Path to executable for zig: ', vim.fn.getcwd() .. '/zig-out/bin/', 'file')
+          end,
           cwd = "${workspaceFolder}",
           stopOnEntry = false,
           args = {},
