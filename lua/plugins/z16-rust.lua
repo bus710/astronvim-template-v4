b013ed313e94d60cb58a6c3657d4cd3bc2db3630
bus710 <bus710@gmail.com>
2024-09-09 20:23:17 -0700

    progress

--- lua/plugins/z16-rust.lua
+++ lua/plugins/z16-rust.lua
@@ -16,6 +16,34 @@ If Mason keeps failing codelldb, delete the lock file with
 ]]--
 
 
+local dap = require "dap"
+local dapui = require "dapui"
+
+function getcwdtail()
+  return vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+end
+
+-- Rustaceanvim will take care of this part
+-- dap.adapters.codelldb = {
+--
+-- }
+
+dap.configurations.rust = {
+  {
+    name = 'CodeLLDB: Launch Custom',
+    type = 'codelldb',
+    request = 'launch',
+    program = "${workspaceFolder}/target/debug/" .. getcwdtail(),
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+    args = {},
+    runInTerminal = false,
+  },
+}
+
+dap.listeners.after.event_initialized["dapui_config"] = function() dapui.open {} end
+dap.listeners.before.event_terminated["dapui_config"] = function() dapui.close {} end
+dap.listeners.before.event_exited["dapui_config"] = function() dapui.close {} end
 
 return {
   -- If RustAnalyzer crashes
