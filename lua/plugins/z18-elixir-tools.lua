a6afcc74b315dba16c0107c16b2fa75b42950e16
bus710 <bus710@gmail.com>
2024-09-19 11:47:04 -0700

    progress

--- lua/plugins/z18-elixir-tools.lua
+++ lua/plugins/z18-elixir-tools.lua
@@ -2,6 +2,33 @@
 
 -- This function will run when open an elixir file.
 
+
+
+
+
+local dap = require "dap"
+local elixir_ls_debugger = vim.fn.exepath "elixir-ls-debugger"
+
+if elixir_ls_debugger ~= "" then
+  dap.adapters.mix_task = {
+    type = "executable",
+    command = elixir_ls_debugger,
+  }
+
+  dap.configurations.elixir = {
+    {
+      type = "mix_task",
+      name = "phoenix server",
+      task = "phx.server",
+      request = "launch",
+      projectDir = "${workspaceFolder}",
+      exitAfterTaskReturns = false,
+      debugAutoInterpretAllModules = false,
+    },
+  }
+end
+
+
 return {
   {
     "elixir-tools/elixir-tools.nvim",
