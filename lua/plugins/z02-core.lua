90ca8469992737e8a7c3e0cb42fae57ace759377
bus710 <bus710@gmail.com>
2025-01-31 20:13:36 -0800

    progress

---
+++ lua/plugins/z02-core.lua
@@ -0,0 +1,26 @@
+-- https://www.reddit.com/r/AstroNvim/comments/1bu8dx6/astronvim_v4_single_file_conf/
+
+return {
+  { 'Mofiqul/dracula.nvim' },
+  {
+    "AstroNvim/astroui",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+  {
+    "AstroNvim/astrocore",
+    opts = {
+      options = {
+        opt = {
+          tabstop = 4,
+          softtabstop = 4,
+          -- colorcolumn = '80,120'
+          colorcolumn = '120',
+          -- textwidth = 100,
+          wrap = true
+        },
+      },
+    },
+  },
+}
