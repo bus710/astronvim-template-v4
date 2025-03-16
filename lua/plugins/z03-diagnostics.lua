ab4a403156e781dc38b4d76d2ea84ebcca5078de
Seongjun Kim <bus710@gmail.com>
2025-03-16 16:41:11 -0700

    progress

--- lua/plugins/z03-diagnostics.lua
+++ lua/plugins/z03-diagnostics.lua
@@ -6,25 +6,27 @@ return {
     ---@type AstroCoreOpts
     opts = {
       mappings = {
-        -- first key is the mode
+        -- first key is the mode - n is the normal mode
         n = {
           gn = {
             function()
-              vim.keymap.set(
-                "n",
-                "gn",
-                function()
-                  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
-                end)
+              -- vim.keymap.set(
+              --   "n",
+              --   "gn",
+              --   function()
+              --     vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+              --   end)
+                vim.diagnostic.enable(not vim.diagnostic.is_enabled())
             end,
             desc = "Disable vim diagnostic info",
           },
-          gm = {
+          ga = {
             function()
-              vim.keymap.set("n", "gm", vim.diagnostic.enable)
+              vim.cmd("Cscope db add /home/bus710/repo/module-test/linux/cscope.out")
+              vim.cmd("set tags=/home/bus710/repo/module-test/linux/tags")
             end,
-            desc = "Enable vim diagnostic info",
-          }
+            desc = "Add kernel symbol files",
+          },
         },
       },
     },
