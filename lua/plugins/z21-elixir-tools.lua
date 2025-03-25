af90963f735d6864329fd62e6123a255ef8de2e5
Seongjun Kim <bus710@gmail.com>
2025-03-24 21:34:07 -0700

    progress

--- lua/plugins/z21-elixir-tools.lua
+++ lua/plugins/z21-elixir-tools.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- This function will run when open an elixir file.
 
